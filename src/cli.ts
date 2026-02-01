43b67fed1ae5347a51e1d354fcc7027b976bd183
lonewolfyx <877085893@qq.com>
2026-02-01 23:47:05 +0800

    feat(cli): implement junk files and directories cleanup functionality

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,45 @@
+import type { IOptions } from '@/types.ts'
+import * as process from 'node:process'
 import cac from 'cac'
+import { glob } from 'glob'
+import { rimraf } from 'rimraf'
+import { resolveConfig } from '@/config.ts'
+import { JUNK_DIRS, JUNK_FILES } from '@/constants.ts'
 import { name, version } from '../package.json'
 
 const cli = cac(name)
 
 cli.command('')
-    .option('--test -t', 'arg description', { default: false })
-    .action(() => {
+    .option('-c,--cwd <path>', 'working directory', { default: process.cwd() })
+    .action(async (options: IOptions) => {
+        const config = resolveConfig(options)
+
+        const patterns = [
+            ...JUNK_DIRS.map(d => `**/${d}/`),
+            ...JUNK_FILES.map(f => `**/${f}`),
+        ]
+
+        const folders = await glob(patterns, {
+            cwd: config.cwd,
+            absolute: true,
+            nodir: false,
+            ignore: {
+                ignored: (p) => {
+                    // If the path already contains node_modules and does not end with node_modules, it is going deeper and can be ignored
+                    const pathString = p.fullpath()
+                    return pathString.includes('node_modules/') && !pathString.endsWith('node_modules/')
+                },
+            },
+        })
+
+        if (folders.length > 0) {
+            await Promise.all(folders.map(async (folder) => {
+                console.log(folder.replace(config.cwd, ''))
+                await rimraf(folder)
+            }))
+        }
+
+        console.log('Done.')
     })
 
 cli.help()
---
+++ src/constants.ts
@@ -0,0 +1,29 @@
+export const JUNK_DIRS = [
+    'node_modules',
+    'jspm_packages',
+    'web_modules',
+    '.cache',
+    'dist',
+    '.nuxt',
+    '.parcel-cache',
+    '.next',
+    'out',
+    '.vuepress/dist',
+    '.temp',
+    '.svelte-kit',
+    '.docusaurus',
+    '.serverless',
+    '.fusebox',
+    '.dynamodb',
+    '.firebase',
+    '.tern-port',
+]
+
+export const JUNK_FILES = [
+    'package-lock.json', // npm
+    'pnpm-lock.yaml', // pnpm
+    'yarn.lock', //  yarn
+    'bun.lockb', // bun
+    'deno.lock', // deno
+    'vlt.json', // vlt
+]
